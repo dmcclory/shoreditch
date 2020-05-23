f26fa3323bfbcc161c57d79f6402dc8084fc9657
Dan McClory <danmcclory@gmail.com>
2020-05-23 18:44:10 -0400

    able to go back N weeks or N months

--- make_lines.py
+++ make_lines.py
@@ -2,7 +2,7 @@ import pickle
 from sparklines import sparklines
 from itertools import groupby
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import defaultdict
 
 
@@ -17,21 +17,46 @@ with open('data.pickle', 'rb') as f:
     thing = pickle.load(f)
 
 
-def get_linedata(pings):
+def get_week_keys_for_n_months(n):
+    today = datetime.now()
+    res = []
+    for i in range(n):
+        t = today - timedelta(7*i)
+        res.append((t.year, t.month, t.day // 7))
+
+    res.reverse()
+    return res
+
+
+def get_month_keys_for_n_months(n):
+    today = datetime.now()
+    res = []
+    for i in range(n):
+        t = today - timedelta(30*i)
+        res.append((t.year, t.month))
+
+    res.reverse()
+    return res
+
+
+
+def get_linedata(pings, weeks=False, months=False, go_back=6):
+    if weeks == True:
+        keys = get_week_keys_for_n_months(go_back)
+        counts = groupby(pings, lambda d: (d.year, d.month, d.day // 7))
+    else:
+        keys = get_month_keys_for_n_months(go_back)
+        counts = groupby(pings, lambda d: (d.year, d.month))
     window = defaultdict(lambda: 0)
-    # want to make this generalizable
-    # for the multiple year case
-    # or how do we do the past N months, say??
-    # the lambda should probably always include the year.....
-    counts_per_month_week = groupby(pings, lambda d: (d.month, d.day // 7))
-    for (k, g) in counts_per_month_week:
+
+    for (k, g) in counts:
         window[k] = len(list(g))
-    linedata = [ window[(j, i)] for j in range(0, 6) for i in range(0, 6) ]
+    linedata = [ window[key] for key in keys]
     return linedata
 
 
-number_to_see = 20
-datasets = [get_linedata(thing[i].pings) for i in range(0, number_to_see)]
+number_to_see = 30
+datasets = [get_linedata(thing[i].pings, weeks=True, go_back=15) for i in range(0, number_to_see)]
 maximum = max([item for sublist in datasets for item in sublist])
 
 lines = [ sparklines(d, minimum = 0, maximum = maximum + 1)[0] for d in datasets]
