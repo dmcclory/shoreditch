c8defdbcc55d92c8a0e3e695458d5de0d067a900
Dan McClory <danmcclory@gmail.com>
2020-05-28 00:19:42 -0400

    nice refactoring

--- make_lines.py
+++ make_lines.py
@@ -7,11 +7,7 @@ from collections import defaultdict
 
 import sys
 
-# gotta share/import this
-class Entry():
-    def __init__(self):
-        self.pings = []
-        self.titles = []
+from termcolor import colored
 
 WINDOW = sys.argv[1] or 'weeks'
 GO_BACK = int(sys.argv[2])  if len(sys.argv) > 2 else 30
@@ -79,15 +75,39 @@ with open('data.pickle', 'rb') as f:
 # and of course ... filtering on "seen"
 # and being able to see reports of things I *have* seen
 
-window = sys.argv[1] or 'weeks'
-go_back = int(sys.argv[2])  if len(sys.argv) > 2 else 30
-
-# number_to_see = 40
-# datasets = [get_linedata(thing[i].pings, window=window, go_back=go_back) for i in range(0, number_to_see)]
 datasets = [thing[i].get_dataset() for i in range(0, NUMBER_TO_SEE)]
 maximum = max([item for sublist in datasets for item in sublist])
 
-lines = [ sparklines(d, minimum = 0, maximum = maximum + 1)[0] for d in datasets]
-titles = [thing[i].titles[0] for i in range(0, number_to_see)]
-for (title, line) in zip(titles, lines):
-    print(title.ljust(40) + line)
+finished = ["Burning", "Jojo Rabbit", "Game of Thrones (s2)", "Uncut Gems", "Babylon Berlin (s3)", "Jaws", "Knives Out", "Barry (s2)", "Germania", "Force Majeure"]
+started = ["Bosch", "Satoshi Kon Filmography", "Death Stranding", "Wolf Hall", "Riverdale"]
+
+
+def build_sparkline(piece):
+    dataset = piece.get_dataset()
+    return sparklines(dataset, minimum = 0, maximum = maximum + 1)[0]
+
+
+def title_format(piece):
+    title = piece.titles[0]
+    return title + " (" + str(len(piece.pings)) + ")"
+
+
+def color_for_row(piece):
+    title = piece.titles[0]
+    if title in finished:
+        color = "green"
+    elif title in started:
+        color = "cyan"
+    else:
+        color = "white"
+    return color
+
+
+selected_rows = [thing[i] for i in range(NUMBER_TO_SEE)]
+
+lines = [ build_sparkline(p) for p in selected_rows]
+titles = [title_format(p) for p in selected_rows]
+colors_from_statuses = [color_for_row(p) for p in selected_rows]
+
+for (title, line, color) in zip(titles, lines, colors_from_statuses):
+    print(colored( title.ljust(40) + line, color))
