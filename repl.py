bb11493ef95e40a44b8fea669869f795a4837f2b
Dan McClory <danmcclory@gmail.com>
2020-09-03 11:33:25 -0400

    make repl more userfriendly (for me)

--- repl.py
+++ repl.py
@@ -1,10 +1,14 @@
+import os
 import pickle
 
 from entry import Entry
 
+PICKLE_PATH = os.environ['PICKLE_PATH']
 
-with open('data.pickle', 'rb') as f:
-    thing = pickle.load(f)
+with open(PICKLE_PATH, 'rb') as f:
+    data = pickle.load(f)
+
+print("'data' holds {} records. have fun repl-in!".format(len(data)))
 
 import pdb; pdb.set_trace()
 print("done!")
