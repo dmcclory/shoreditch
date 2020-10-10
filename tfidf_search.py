9df591ecef9e65e75d3526b550bac9ad838d9614
Dan McClory <danmcclory@gmail.com>
2020-10-10 16:33:30 -0400

    use the seasons as part of the key

--- tfidf_search.py
+++ tfidf_search.py
@@ -75,8 +75,8 @@ def get_matches_df(sparse_matrix, name_vector, top=None):
 
 
 
-def extract_annotations(df, column):
-    return df[column].str.extract(r'.*\((.*)\)\w*$')
+def extract_annotations(series):
+    return series.str.extract(r'.*\((.*)\)\w*$')[0].fillna('')
 
 def chop_annotations(series):
     return series.str.replace(r'\((.*)\)\w*$', '')
@@ -86,13 +86,22 @@ STOP_WORDS = ['in', 'on', 'the', 'of']
 def remove_stop_words(series):
     return series.str.split(' ').apply(lambda l: ' '.join(w for w in l if w not in STOP_WORDS))
 
+def add_season_suffix(keys, suffixes):
+    x = pd.DataFrame.from_dict({'protokey': keys, 'annotation': suffixes}, dtype='string')
+    return x.apply(
+        lambda r: r['protokey'] + ' ' + r['annotation'] if re.match(r'^s\d+$', r['annotation']) else r['protokey'], axis=1
+    )
+
 def normalize(df):
     column_label = 'title'
+    annotations = extract_annotations(df[column_label])
     chopped = chop_annotations(df[column_label])
     stripped = chopped.str.strip()
     lower = stripped.str.lower()
     destopped = remove_stop_words(lower)
-    df['normalized'] =  destopped
+    seasoned = add_season_suffix(destopped, annotations)
+    df['normalized'] =  seasoned
+    df['annotations'] = annotations
     return df
 
 
