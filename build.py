1dec9bbe6fe207490d162f66601b0c0296ee1860
Dan McClory <danmcclory@gmail.com>
2020-10-11 01:02:52 -0400

    move the rest of the code out of match_watch_log into build

--- build.py
+++ build.py
@@ -1,4 +1,16 @@
-from shoreditch.tfidf_search import TfidfSearcher
+from shoreditch.tfidf_search import TfidfSearcher, process_watch_log, add_watches_to_entries
+from shoreditch.persistence import store_database
 
 searcher = TfidfSearcher(['data/2020.txt', 'data/2019.txt', 'data/2018.txt'])
-searcher.save()
+
+
+as_watches, uncategorized = process_watch_log(searcher.matches_df, ['2020', '2019'])
+entries = add_watches_to_entries(as_watches, searcher.entries)
+
+path = 'augmented_data.pickle'
+store_database(path, entries)
+
+print(as_watches)
+print(len(as_watches.keys()))
+print('---------------')
+print(uncategorized)
