d75d0da343591d4d1a28b2e55c77e3898979d893
Dan McClory <danmcclory@gmail.com>
2020-10-10 23:19:34 -0400

    move the search class

--- build.py
+++ build.py
@@ -1,5 +1,4 @@
-import pandas as pd
-from tfidf_search import TfidfSearcher
+from shoreditch.tfidf_search import TfidfSearcher
 
 searcher = TfidfSearcher(['data/2020.txt', 'data/2019.txt', 'data/2018.txt'])
 searcher.save()
