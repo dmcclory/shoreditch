5307d65e63a0b3286f896379bc83bf04060c475d
Dan McClory <danmcclory@gmail.com>
2020-10-10 21:31:02 -0400

    bon voyage LinkageSearch ... you helped me define the interfaces, but now your watch is ended

--- linkage_lines.py
+++
@@ -1,8 +0,0 @@
-
-
-from linkage_search import LinkageSearcher 
-
-searcher = LinkageSearcher(['data/2020.txt', 'data/2019.txt', 'data/2018.txt'])
-# print (searcher.all_titles[0])
-
-searcher.save()
